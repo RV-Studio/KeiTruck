bab13dd62b9730ef6d20024af04385e41fd63dca
Vivid-Hornet <85631937+JordanTeasdale@users.noreply.github.com>
2023-04-13 21:59:00 -0400

    Setup Project

---
+++ KeiTruckSimulator/Source/KeiTruckSimulator/KeiTruckSimulator.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "KeiTruckSimulator.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KeiTruckSimulator, "KeiTruckSimulator" );
+ 
